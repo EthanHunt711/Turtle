84acb65d2e97b11237ccd5bd7d98173a3a156aa9
EthanHunt711 <79079092+EthanHunt711@users.noreply.github.com>
2021-09-09 10:57:21 +0200

    New for today

---
+++ W2 - Frame.py
@@ -0,0 +1,10 @@
+def frame(text):
+    text_length = len(text)
+    print('**' + text_length*'*' + '**')
+    #print()
+    print('*', text, '*')
+    #print()
+    print('**' + text_length * '*' + '**')
+
+
+frame('Det här är ett test')
---
+++ W2 - Friut.py
@@ -0,0 +1,29 @@
+import math
+from math import pi
+
+
+def circle_area(radius):
+    return pi * radius ** 2
+
+
+def sphere_volume(radius):
+    return 4.0 / 3.0 * pi * radius ** 3
+
+
+def triangle_area(base, height):
+    return (base * height) / 2
+
+
+def friut_val(fruit):
+    if fruit == 'banana':
+        return 10
+    elif fruit == 'apple':
+        return 2
+    elif len(fruit) < 6:
+        return 4
+    else:
+        return 1
+
+
+circle_area(30)
+sphere_volume(30)
