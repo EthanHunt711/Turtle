861d99599eb8fdd6d2e1cfbdc72f94ed13ccfeee
EthanHunt711 <79079092+EthanHunt711@users.noreply.github.com>
2021-09-20 12:18:12 +0200

    edited w2

--- W2 - root.py
+++ W2 - root.py
@@ -2,12 +2,12 @@ def find_root(number):
     guess = 1.0
     for x in range(1, 11):
         guess = ((number / guess) + guess) / 2
-        print('after iteration', x, 'my guess is', guess)
+        print('After iteration', x, 'my guess is', guess)
 
 
 def main():
-    user_guess = float(input('please enter a random number:'))
+    user_guess = float(input("please enter a random number: "))
     print(find_root(user_guess))
 
 
-find_root(17)
+main()
