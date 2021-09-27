eb52e05e124846de33fbe14f96d3c9aaf9972b73
EthanHunt711 <79079092+EthanHunt711@users.noreply.github.com>
2021-09-27 18:18:50 +0200

    completed w3

--- W3 - secret.py
+++ W3 - secret.py
@@ -1,10 +1,10 @@
 import random
 
 
-def secret_number():
+def game():
     count_guess = 0
     secret_num = random.randrange(1, 99, 1)
-    guess_num = 'what is your guess? '
+    guess_num = "Let's play a fun game! \nI have thought of number between 1 and 99 \nDo you think you can guess what my secret number is? "
     next_guess = 'please guess again! '
     low_guess = 'That is too low'
     high_guess = 'That is too high'
@@ -15,7 +15,7 @@ def secret_number():
 
     guess = int(input(guess_num))
 
-    while guess:
+    while guess != secret_num:
         count_guess += 1
         if guess < secret_num:
             print(low_guess)
@@ -24,22 +24,24 @@ def secret_number():
         elif guess > secret_num:
             print(high_guess)
             guess = int(input(next_guess))
-        elif guess == secret_num:
-            print(correct_guess)
-            print('You made it in', count_guess, 'guesses.')
-            if count_guess <= 3:
-                print(good_guess)
-                break
-            elif count_guess > 4 < 6:
-                print(after_four_guesses)
-                break
-            elif count_guess > 6:
-                print(bad_guess)
-                break
+    else:
+        print(correct_guess)
+        print('You made it in', count_guess, 'guesses.')
+        evaluation(count_guess)
 
 
-def game():
-    secret_number()
+def evaluation(number):
+    good_guess = 'That is really good'
+    after_four_guesses = 'You can do better!'
+    bad_guess = 'It took you a long time but you made it at last'
+
+    if number <= 3:
+        print(good_guess)
+
+    elif number > 4 < 6:
+        print(after_four_guesses)
+
+    elif number > 6:
+        print(bad_guess)
 
 
-game()
