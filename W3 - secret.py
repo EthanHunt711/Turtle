d0bb2adbafc8d3bc4df0a2ed7f67028f074f52db
EthanHunt711 <79079092+EthanHunt711@users.noreply.github.com>
2021-09-28 18:41:54 +0200

    completed w3

--- W3 - secret.py
+++ W3 - secret.py
@@ -4,14 +4,13 @@ import random
 def game():
     count_guess = 0
     secret_num = random.randrange(1, 99, 1)
-    guess_num = "Let's play a fun game! \nI have thought of number between 1 and 99 \nDo you think you can guess what my secret number is? "
+    guess_num = "Let's play a fun game! " \
+                "\nI have thought of number between 1 and 99 " \
+                "\nDo you think you can guess what my secret number is? "
     next_guess = 'please guess again! '
     low_guess = 'That is too low'
     high_guess = 'That is too high'
     correct_guess = 'Congratulations! That is correct'
-    good_guess = 'That is really good'
-    after_four_guesses = 'You can do better!'
-    bad_guess = 'It took you a long time but you made it at last'
 
     guess = int(input(guess_num))
 
@@ -38,10 +37,10 @@ def evaluation(number):
     if number <= 3:
         print(good_guess)
 
-    elif number > 4 < 6:
+    elif number < 6:
         print(after_four_guesses)
 
-    elif number > 6:
+    else:
         print(bad_guess)
 
 
--- W3 - week3.py
+++ W3 - week3.py
@@ -4,8 +4,6 @@ import random
 def odd_part(n):
     while n % 2 == 0:
         n = n // 2
-    else:
-        n
     return n
 
 
@@ -16,8 +14,6 @@ def count_rand(limit):
     while total < limit:
         random_numbers_count += 1
         total += n
-        if total >= limit:
-            total
 
     return random_numbers_count
 
@@ -26,7 +22,8 @@ def first_short(words):
     for short_word in words:
         if len(short_word) < 5:
             return short_word
-
+    else:
+        return ''
 
 
 
