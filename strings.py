c4d6d5e33364dee2806cd185c5a36dc755eb73a4
EthanHunt711 <79079092+EthanHunt711@users.noreply.github.com>
2021-09-29 18:49:15 +0200

    not so good in w4

--- strings.py
+++ strings.py
@@ -4,3 +4,52 @@ def indef(text):
         return 'an ' + text
     else:
         return 'a ' + text
+
+
+def parens(string):
+    alphabets = []
+    for letter in string:
+        alphabets.append(letter)
+    print(alphabets, sep=", ")
+
+
+def three_rays(string):
+    count = 0
+    for letter in string:
+        count += 1
+        print(letter + count*'-' + letter + count*'-' + letter)
+
+
+def swap(s):
+    if len(s) % 2 == 0:
+        s_half = len(s)//2
+        return s[s_half:]+s[:s_half]
+    else:
+        return s
+
+
+def first_digit(string):
+    string_char = []
+    for letter in string:
+        string_char.append(letter)
+        if letter.isdigit():
+            return letter
+    else:
+        return 0
+
+
+def numbers_for_uc(string):
+    for index, character in enumerate(string):
+        if character.isupper():
+            return string.replace(character, f'[{str(index)}]')
+    return string
+
+
+def grid(s):
+    n = 0
+    for i in s:
+        while len(s):
+            s.startwirh(n+i)
+
+
+
