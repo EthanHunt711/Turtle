23a166899484589d25a86235187b91e9fdd8a4f4
EthanHunt711 <79079092+EthanHunt711@users.noreply.github.com>
2021-10-02 15:47:01 +0200

    w4 nearly complete

---
+++ harshad.py
@@ -0,0 +1,16 @@
+def digit_sum(d):
+    total = 0
+    for i in str(d):
+        total = int(i) + total
+    return total
+
+
+def is_harshad(n):
+    if n % digit_sum(n) == 0:
+        return True
+    return False
+
+
+for i in range(1, 201):
+    if is_harshad(i) is True:
+        print(i)
