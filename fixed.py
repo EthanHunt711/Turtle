8ec1aa7203877aac027385bcd2195ca8d1f5e04f
EthanHunt711 <79079092+EthanHunt711@users.noreply.github.com>
2021-10-04 08:26:14 +0200

    w4 complete

--- fixed.py
+++ fixed.py
@@ -31,5 +31,14 @@ def countmatch(s1, s2):
     for i, char in enumerate(s1):
         s1 = s1.lower()
         s2 = s2.lower()
+        print(i, char)
         if char != s2[i]:
             return i
+        elif s1 == s2:
+            return len(s1)
+        elif s2.startswith(f'{s1}'):
+            return len(s1)
+        elif s1.startswith(f'{s2}'):
+            return len(s2)
+
+
