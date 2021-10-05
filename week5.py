0e236ee966a58f35bbf23f1bc273b36b7346bb3e
EthanHunt711 <79079092+EthanHunt711@users.noreply.github.com>
2021-10-05 15:54:51 +0200

    w5 begins

--- week5.py
+++ week5.py
@@ -12,8 +12,11 @@ def find_longer(words, n):
 
 
 def histogram(numbers, char):
+    row = 0
     for n in numbers:
-        print(n * char)
+        row = n * char
+    return row
+
 
 
 def allit(words):
@@ -24,11 +27,43 @@ def allit(words):
         alliterate = True
     elif len(words) > 1:
         first_letter = words[0][:1]
-        for w in words:
-            if w.startswith(first_letter):
+        for l in words:
+            if l.startswith(first_letter):
                 alliterate = True
             else:
                 alliterate = False
     return alliterate
+
+
+def anti_allit(words):
+    first_letter = words[0][:1]
+    for l in words:
+        if not l.startswith(first_letter):
+            return True
+    return False
+
+
+def addpairs(numbers):
+    result = []
+    for i, n in enumerate(numbers):
+        for i in numbers:
+            if i < (len(numbers)-1):
+                result.append(n + numbers[i+1])
+    print(result)
+
+
+def endings(words):
+    endings_list = [words]
+    for c in words:
+        if len(words) > 0:
+            words = words.strip(words[:1])
+            endings_list.append(words)
+    return endings_list
+
+
+def vhistogram(numbers, char):
+    print ('no')
+
+
 
 
