8ef25e4cc92a890f41fe764875e6ccda9ad041ce
EthanHunt711 <79079092+EthanHunt711@users.noreply.github.com>
2021-12-08 16:48:35 +0100

    Probably the end

--- gutenberg.py
+++ gutenberg.py
@@ -5,11 +5,11 @@ def firstdict(corpus):
     firsts = {}
     for sent in corpus:
         first_word = sent[0].lower()
-        if first_word.isalpha():
-            if first_word in firsts:
-                firsts[first_word] += 1
-            else:
-                firsts[first_word] = 1
+        #if first_word.isalpha():
+        if first_word in firsts:
+            firsts[first_word] += 1
+        else:
+            firsts[first_word] = 1
     return firsts
 
 
@@ -18,7 +18,7 @@ def find_surprising(filename):
     freq1 = firstdict(corpus)
     for sent in corpus:
         if surprising(sent, freq1):
-            print(sent)
+            print(f"{' '.join(sent[:-1])}{sent[-1]}")
 
 
 def surprising(sent, freq1):
@@ -26,13 +26,13 @@ def surprising(sent, freq1):
     if len(sent) >= 3:
         if sent[0] in freq1.keys():
             for word in sent[1:]:
-                if word in freq1.keys():
-                    if freq1[sent[0]] < freq1[word]:
-                        surp = True
-                    else:
-                        surp = False
+                #if word.isalpha():
+                if freq1[sent[0]] > freq1.get(word, 0):
+                    surp = False
+                else:
+                    surp = True
     return surp
 
 
 find_surprising('austen-emma.txt')
-find_surprising('blake-poems.txt')
+# find_surprising('blake-poems.txt')
