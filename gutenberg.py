0741ee5340671a6ea0d8c2807e36c64891d7f234
EthanHunt711 <79079092+EthanHunt711@users.noreply.github.com>
2021-12-15 01:21:25 +0100

    Probably the end

--- gutenberg.py
+++ gutenberg.py
@@ -5,11 +5,11 @@ def firstdict(corpus):
     firsts = {}
     for sent in corpus:
         first_word = sent[0].lower()
-        #if first_word.isalpha():
-        if first_word in firsts:
-            firsts[first_word] += 1
-        else:
-            firsts[first_word] = 1
+        if first_word.isalpha():
+            if first_word in firsts:
+                firsts[first_word] += 1
+            else:
+                firsts[first_word] = 1
     return firsts
 
 
@@ -17,22 +17,24 @@ def find_surprising(filename):
     corpus = gutenberg.sents(filename)
     freq1 = firstdict(corpus)
     for sent in corpus:
-        if surprising(sent, freq1):
+        if surprising(sent, freq1) is True:
             print(f"{' '.join(sent[:-1])}{sent[-1]}")
 
 
 def surprising(sent, freq1):
-    surp = False
+    surprising_sent = False
+    freq_list = []
     if len(sent) >= 3:
-        if sent[0] in freq1.keys():
-            for word in sent[1:]:
-                #if word.isalpha():
-                if freq1[sent[0]] > freq1.get(word, 0):
-                    surp = False
-                else:
-                    surp = True
-    return surp
+        if sent[0].isalpha():
+            for word in sent:
+                if word.isalpha():
+                    freq_list.append(freq1.get(word.lower(), 0))
+            if freq_list[0] < min(freq_list[1:]):
+                surprising_sent = True
+            else:
+                surprising_sent = False
+    return surprising_sent
 
 
 find_surprising('austen-emma.txt')
-# find_surprising('blake-poems.txt')
+# find_surprising('chesterton-ball.txt')
